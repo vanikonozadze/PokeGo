c6504b0208fb8a58cd9af635ab7636e97ba8ae13
Vaniko Nozadze <ivane.nozadze.1@btu.edu.ge>
2024-02-06 23:18:25 +0400

    added search bar and filter function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
+import { PokemonService } from './services/pokemon.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'PokeGo';
+  pokemonArray: any[] = [];
+  fetchedArray: any[] = [];
+  searchItem: string = '';
+
+  constructor(private service: PokemonService) {}
+
+  ngOnInit(): void {
+    this.fetchPokemons();
+  }
+
+  fetchPokemons() {
+    this.service.getPokemons().subscribe({
+      next: (pokemons) => {
+        this.pokemonArray = pokemons.results;
+        this.fetchedArray = pokemons.results;
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+
+  filterPokemons(): void {
+    if (this.searchItem) {
+      this.pokemonArray = this.fetchedArray.filter((pokemon) =>
+        pokemon.name.toLowerCase().startsWith(this.searchItem.toLowerCase())
+      );
+    } else {
+      this.pokemonArray = this.fetchedArray;
+    }
+  }
 }
---
+++ src/app/services/pokemon.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PokemonService {
+  baseApiUrl: string = 'https://pokeapi-proxy.freecodecamp.rocks/api/pokemon';
+  constructor(private http: HttpClient) {}
+
+  getPokemons(): Observable<any> {
+    return this.http.get<any>(this.baseApiUrl);
+  }
+}
